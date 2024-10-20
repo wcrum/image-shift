93341ae63dff973d8beb406a591fac568aeb6b77
Will <will@wcrum.dev>
2024-10-20 16:17:31 -0600

    add regex swap

--- imageswap.go
+++ imageswap.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
 
 	"github.com/google/go-containerregistry/pkg/name"
@@ -25,6 +27,7 @@ type ImageMapping struct {
 	Registry string `yaml:"registry"`
 	Action   string `yaml:"action"`
 	Target   string `yaml:"target"`
+	Image    string `yaml:"image"`
 }
 
 func (i *ImageSwap) SwapImage(image string) string {
@@ -33,11 +36,11 @@ func (i *ImageSwap) SwapImage(image string) string {
 	registry := ref.Context().RegistryStr()
 
 	// if registry == default registry return image
-	if string(registry) == i.Default {
-		return ref.Name()
-	}
-
 	var newImage string
+
+	if string(registry) == i.Default {
+		newImage = ref.Name()
+	}
 
 	for _, m := range i.Mappings {
 		switch m.Action {
@@ -58,6 +61,29 @@ func (i *ImageSwap) SwapImage(image string) string {
 					newImage = fmt.Sprintf("%s/%s", m.Target, ref.Context().RepositoryStr())
 				}
 			}
+		case "exact-swap":
+			if m.Image == ref.String() {
+				newImage = m.Target
+			}
+		case "regex-swap":
+			fmt.Println(m.Registry)
+
+			re := regexp.MustCompile(m.Registry)
+
+			match := re.FindStringSubmatch(ref.String())
+			if match != nil {
+				newRepository := strings.Replace(ref.String(), match[0], "", -1)
+
+				newImage = m.Target
+
+				if len(match) > 1 {
+					for m := 1; m < len(match); m++ {
+						newImage = strings.Replace(newImage, "$"+strconv.Itoa(m), match[m], -1)
+						newImage = fmt.Sprintf("%s%s", newImage, newRepository)
+					}
+				}
+			}
+
 		}
 	}
 
